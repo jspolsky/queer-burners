1eca86aa1d3ac950763fa9f888893c4932dcb415
Joel Spolsky <joel@spolsky.com>
2020-06-07 13:03:27 -0400

    Adding the build output directory to git

---
+++ web/build/precache-manifest.f0d8c3217d88871a5e7e4c6b2a1e52d8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c3fb3e1757c8cbf239bbaf7d405a6ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7bc5755f75f91bcc299d",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "0502d2470ba92bb65efe",
+    "url": "/static/css/main.4e956cb1.chunk.css"
+  },
+  {
+    "revision": "7bc5755f75f91bcc299d",
+    "url": "/static/js/2.b8e5b675.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.b8e5b675.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0502d2470ba92bb65efe",
+    "url": "/static/js/main.16935947.chunk.js"
+  },
+  {
+    "revision": "40d66643441b3475de99",
+    "url": "/static/js/runtime-main.1c1d9999.js"
+  },
+  {
+    "revision": "859a41ad88a4387246cb194f835f3ed0",
+    "url": "/static/media/header_logo.859a41ad.png"
+  },
+  {
+    "revision": "fb4433e3be0f1dff25cf31ef65b928f6",
+    "url": "/static/media/social_email.fb4433e3.svg"
+  },
+  {
+    "revision": "51d5c4a3d8fbd9afb0111ec26469cdf0",
+    "url": "/static/media/social_facebook.51d5c4a3.svg"
+  },
+  {
+    "revision": "76090faf5e945fbd6a82790171e27ef5",
+    "url": "/static/media/social_instagram.76090faf.svg"
+  },
+  {
+    "revision": "abfbd4d99d3eb68a202695654eeccf9c",
+    "url": "/static/media/social_twitter.abfbd4d9.svg"
+  },
+  {
+    "revision": "f83690addf3a445a1f379acd988c4615",
+    "url": "/static/media/social_www.f83690ad.svg"
+  }
+]);
