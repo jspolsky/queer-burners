afccf982f9e5044b274d88c676cd2852c11cdc03
Joel Spolsky <joel@spolsky.com>
2020-06-15 14:10:45 -0400

    Got local testing of lambda functions working

---
+++ aws/local-sam.sh
@@ -0,0 +1,4 @@
+# replace the local version of the layer with a version from the server, because
+# AWS bug, cf https://stackoverflow.com/questions/59325699/aws-layer-code-not-found-when-using-sam-invoke-local
+sed s/\!Ref\ NodeModulesLayer/arn\:aws\:lambda\:us-east-2\:888526606788\:layer\:NodeModulesLayer\:8/ < template.yaml > templatelocal.yaml
+sam local start-api -t templatelocal.yaml -p 3001
