4c69edd5e699398a051eade72977fd2eb2ce1d78
Joel Spolsky <joel@spolsky.com>
2020-06-25 22:18:30 -0400

    prettier logon experience

--- web/src/components/Authenticate.js
+++ web/src/components/Authenticate.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 import { Redirect } from "react-router";
 import { useLocation } from "react-router-dom";
 
 import axios from "axios";
 import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
 import Nav from "react-bootstrap/Nav";
+import Spinner from "react-bootstrap/Spinner";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 import { googleClientId, oauthEndpoint, api } from "../definitions.js";
 import NavDropdown from "react-bootstrap/NavDropdown";
@@ -62,12 +62,6 @@ export const Authenticate = (props) => {
 export const PostAuthenticate = (props) => {
   const [status, setStatus] = useState("Logging you on...");
   const [isLoggedOn, setIsLoggedOn] = useState(false);
-  const [idToken, setIdToken] = useState("");
-  const [fullName, setFullName] = useState("");
-  const [email, setEmail] = useState("");
-  const [isAdmin, setIsAdmin] = useState(false);
-  const [imageUrl, setImageUrl] = useState("");
-  const [duration, setDuration] = useState(0);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -77,12 +71,6 @@ export const PostAuthenticate = (props) => {
           redirect_uri: `${window.location.origin}/postauthenticate`,
         });
 
-        setIdToken(result.data.idToken);
-        setFullName(result.data.name);
-        setEmail(result.data.email);
-        setIsAdmin(result.data.isadmin);
-        setImageUrl(result.data.imageUrl);
-        setDuration(Number(result.data.duration));
         setStatus("Success");
         setIsLoggedOn(true);
         props.OnUserDataChange({
@@ -108,85 +96,19 @@ export const PostAuthenticate = (props) => {
   const queryParams = new URLSearchParams(props.location.search); // call get on that to get individual bits
 
   if (isLoggedOn) {
-    // TODO call a callback from the props so the state is remembered
     return <Redirect to={queryParams.get("state")} />;
   } else {
     return (
-      <div className="App">
-        {status}
-        <Container>
-          <Row>
-            <Col>isLoggedOn</Col>
-            <Col>{isLoggedOn ? "t" : "f"}</Col>
-          </Row>
-          <Row>
-            <Col>idToken</Col>
-            <Col>{idToken}</Col>
-          </Row>
-          <Row>
-            <Col>Full Name</Col>
-            <Col>{fullName}</Col>
-          </Row>
-          <Row>
-            <Col>Email</Col>
-            <Col>{email}</Col>
-          </Row>
-          <Row>
-            <Col>isAdmin</Col>
-            <Col>{isAdmin ? "t" : "f"}</Col>
-          </Row>
-          <Row>
-            <Col>imageUrl</Col>
-            <Col>
-              <img alt="Joel" src={imageUrl}></img>
-            </Col>
-          </Row>
-          <Row>
-            <Col>Duration</Col>
-            <Col>{duration}</Col>
-          </Row>
-          <Row>
-            <Col>Origin</Col>
-            <Col>{window.location.origin}</Col>
-          </Row>
-          <Row>
-            <Col>window.location.href</Col>
-            <Col>{window.location.href}</Col>
-          </Row>
-          <Row>
-            <Col>Search Params</Col>
-            <Col>{props.location.search}</Col>
-          </Row>
-          <Row>
-            <Col>State</Col>
-            <Col>{queryParams.get("state")}</Col>
-          </Row>
-          <Row>
-            <Col>A link to State</Col>
-            <Col>
-              <Link to={queryParams.get("state")}>Link</Link>
-            </Col>
-          </Row>
-          <Row>
-            <Col>Code</Col>
-            <Col>{queryParams.get("code")}</Col>
-          </Row>
-          <Row>
-            <Col>Error</Col>
-            <Col>{queryParams.get("error")}</Col>
-          </Row>
-          <Row>
-            <Col>Hash</Col>
-            <Col>{props.location.hash}</Col>
-          </Row>
-          <Row>
-            <Col>Done?</Col>
-            <Col>
-              <a href="/">Home</a>
-            </Col>
-          </Row>
-        </Container>
-      </div>
+      <Container>
+        <Row>
+          <Col>
+            <Spinner animation="border" role="status">
+              <span className="sr-only"></span>
+            </Spinner>
+            <h2>{status}</h2>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 };
