0c72fb79a446b0c821367b1f6f73d9a35cd294a3
Joel Spolsky <joel@spolsky.com>
2020-06-27 09:28:58 -0400

    header is now functional

--- web/src/components/Header.js
+++ web/src/components/Header.js
@@ -8,56 +8,53 @@ import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import { LinkContainer } from "react-router-bootstrap";
 
-class Header extends React.Component {
-  render() {
-    // TODO the navbar styles are a little strange. Past Years dropdown highlighting for example
-    return (
-      <div>
-        <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
-          <Navbar.Brand href="http://queerburners.com/">
-            Queer Burners
-          </Navbar.Brand>
+export const Header = (props) => {
+  return (
+    <div>
+      <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
+        <Navbar.Brand href="http://queerburners.com/">
+          Queer Burners
+        </Navbar.Brand>
 
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="mr-auto">
-              <LinkContainer to="/">
-                <Nav.Link>Directory</Nav.Link>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <LinkContainer to="/">
+              <Nav.Link>Directory</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/submit">
+              <Nav.Link>Submit</Nav.Link>
+            </LinkContainer>
+            <NavDropdown title="Other Years" id="collasible-nav-dropdown">
+              <LinkContainer to="/year/2019">
+                <NavDropdown.Item>2019 Metamorphoses</NavDropdown.Item>
               </LinkContainer>
-              <LinkContainer to="/submit">
-                <Nav.Link>Submit</Nav.Link>
+              <LinkContainer to="/year/2020">
+                <NavDropdown.Item>2020 The Multiverse</NavDropdown.Item>
               </LinkContainer>
-              <NavDropdown title="Other Years" id="collasible-nav-dropdown">
-                <LinkContainer to="/year/2019">
-                  <NavDropdown.Item>2019 Metamorphoses</NavDropdown.Item>
-                </LinkContainer>
-                <LinkContainer to="/year/2020">
-                  <NavDropdown.Item>2020 The Multiverse</NavDropdown.Item>
-                </LinkContainer>
-                <LinkContainer to="/year/2021">
-                  <NavDropdown.Item>2021</NavDropdown.Item>
-                </LinkContainer>
-              </NavDropdown>
-            </Nav>
-            <Nav>
-              <Authenticate
-                userData={this.props.userData}
-                OnUserDataChange={this.props.OnUserDataChange}
-              />
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-        <Link to="/">
-          <img
-            className="img-fluid w-100"
-            src={banner}
-            alt="Queer Burners"
-            style={{ marginBottom: "2rem" }}
-          />
-        </Link>
-      </div>
-    );
-  }
-}
+              <LinkContainer to="/year/2021">
+                <NavDropdown.Item>2021</NavDropdown.Item>
+              </LinkContainer>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Authenticate
+              userData={props.userData}
+              OnUserDataChange={props.OnUserDataChange}
+            />
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Link to="/">
+        <img
+          className="img-fluid w-100"
+          src={banner}
+          alt="Queer Burners"
+          style={{ marginBottom: "2rem" }}
+        />
+      </Link>
+    </div>
+  );
+};
 
 export default Header;
