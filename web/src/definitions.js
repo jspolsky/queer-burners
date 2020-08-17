95fe1c8e3f2ea815f9d8525e8fa3ff16aa3c79f1
Joel Spolsky <joel@spolsky.com>
2020-08-17 18:09:05 -0400

    oops, amazon moved my api

--- web/src/definitions.js
+++ web/src/definitions.js
@@ -1,6 +1,6 @@
 export const defaultYear = new Date().getFullYear();
 export const api =
-  "https://l374cc62kc.execute-api.us-east-2.amazonaws.com/Prod";
+  "https://wwe0vuykb9.execute-api.us-east-2.amazonaws.com/Prod";
 // "http://localhost:3001"; // for local docker testing ~!
 export const s3images =
   "https://s3.us-east-2.amazonaws.com/queerburnersdirectory.com-images";
