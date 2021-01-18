455ec611c3f20fd57fa6d85b4931ec891b649967
Joel Spolsky <spolsky@fogcreek.com>
2021-01-18 11:58:08 -0500

    use the right user, not "default"

--- aws/deploy-sam.sh
+++ aws/deploy-sam.sh
@@ -1,2 +1,2 @@
-sam package --output-template-file packaged.yaml --s3-bucket queerburnersdirectory.com-api-0001
-sam deploy --template-file packaged.yaml --stack-name queer-burners-stack --capabilities CAPABILITY_IAM
+sam package --profile QueerBurnersUser --output-template-file packaged.yaml --s3-bucket queerburnersdirectory.com-api-0001
+sam deploy --profile QueerBurnersUser --template-file packaged.yaml --stack-name queer-burners-stack --capabilities CAPABILITY_IAM
